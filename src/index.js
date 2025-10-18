0e56e37f9c99682556fd48b48b2b2a501aba20d6
11yashiagrawal <11yashiagrawal@gmail.com>
2025-10-19 00:42:31 +0530

    Added async handler, custom api response and error schema in utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
+
+const port = process.env.PORT || 8000;
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed: ", err);
+  });
