c4642b7d3579cdbc000d4d6591870cc92d4b43a0
11yashiagrawal <11yashiagrawal@gmail.com>
2025-10-27 23:31:07 +0530

    aggregation pipeline for subscribers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
-import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
+import {
+  uploadOnCloudinary,
+  deleteFromCloudinary,
+} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 
@@ -291,7 +294,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
 
   const deleteURL = req.user?.avatar;
   // console.log("deleteURL: ", deleteURL);
-  const deleteResponse=await deleteFromCloudinary(deleteURL);
+  const deleteResponse = await deleteFromCloudinary(deleteURL);
   // console.log("deleteResponse: ", deleteResponse);
 
   const user = await User.findByIdAndUpdate(
@@ -336,6 +339,77 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover Image updated successfully."));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(404, "Usernsme not found or missing.");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        username: 1,
+        fullName: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+  console.log("Result after aggregation pipelines: ", channel);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist.");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "Channel details fetched successfully.")
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -346,4 +420,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema(
   {
+    // who is subscribing
     subscriber: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    // which channels is being subscribed to
     channel: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
